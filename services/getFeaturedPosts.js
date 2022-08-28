b90da0e684592fee7f1c9c95f14b6691f99dcf92
Amersafia <amer.safia321@gmail.com>
2022-08-28 21:41:11 +0300

    updated

--- services/getFeaturedPosts.js
+++ services/getFeaturedPosts.js
@@ -1,6 +1,6 @@
 import { request, gql } from "graphql-request";
 const graphqlAPI =
-  process.env.REACT_APP_NEXT_GRAPHCMS_ENDPOINT;
+    process.env.REACT_APP_NEXT_GRAPHCMS_ENDPOINT ||'https://api-eu-central-1.hygraph.com/v2/cl73af9v53edk01uj7sm3hz0l/master';
 
 const getFeaturedPosts = async () => {
   const query = gql`
