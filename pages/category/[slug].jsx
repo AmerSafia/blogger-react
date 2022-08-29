149bc6fbe10a5caf85a9ec01cf08c77f389c677b
Amersafia <amer.safia321@gmail.com>
2022-08-29 03:12:18 +0300

    update

--- pages/category/[slug].jsx
+++ pages/category/[slug].jsx
@@ -31,7 +31,7 @@ const CategoryPost = ({ posts }) => {
 export default CategoryPost;
 
 
-export const getStaticProps = async({ params:{slug}})=> {
+export const getServerSideProps = async({ params:{slug}})=> {
   const posts = await getCategoryPost(slug);
 
   return {
@@ -39,7 +39,9 @@ export const getStaticProps = async({ params:{slug}})=> {
   };
 }
 
-export async function getServerSideProps() {
+// Specify dynamic routes to pre-render pages based on data.
+// The HTML is generated at build time and will be reused on each request.
+export async function getStaticPaths() {
   const categories = await getCategories();
   return {
     paths: categories.map(({ slug }) => ({ params: { slug } })),
